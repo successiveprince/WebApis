2535d255bdcc80b8b921b2c88b188786e6f21636
successiveprince <prince.kumar1@successive.tech>
2023-12-19 18:24:31 +0530

    Data Validation

---
+++ FirstWebApi/Model/StudentData.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstWebApi.Model
+{
+    public class StudentData
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
